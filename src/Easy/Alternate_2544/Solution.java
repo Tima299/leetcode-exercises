f175b9e4ffa73c49fe7cc820211476fc8a2342ef
Tima299 <117558961+Tima299@users.noreply.github.com>
2025-02-16 00:53:37 +0500

    Salom

--- src/Easy/Alternate_2544/Solution.java
+++ src/Easy/Alternate_2544/Solution.java
@@ -1,7 +1,25 @@
 package Easy.Alternate_2544;
 
-
 public class Solution {
+    public int alternateDigitSum(int n) {
+        int sum = 0, length = 0, temp = n;
+        while (temp > 0) {
+            temp /= 10;
+            length++;
+        }
+        int sign = (length % 2 == 0) ? -1 : 1;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += sign * digit;
+            sign *= -1;
+            n /= 10;
+        }
+        return sum;
+    }
+}
+
+//Friends Solution -> Epammer
+/*public class Solution {
     public int alternateDigitSum(int n) {
         int l = String.valueOf(n).length();
         return r(l % 2 == 0 ? false : true, n, 0);
@@ -15,4 +33,4 @@ public class Solution {
         return r(!s, n /= 10, S + t);
     }
 
-}
+}*/
